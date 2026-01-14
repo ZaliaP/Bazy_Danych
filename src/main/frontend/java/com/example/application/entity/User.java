57935565c32911e0ffed4f1a875d2347b48f314f
Rozalia <157622733+ZaliaP@users.noreply.github.com>
2026-01-15 00:38:03 +0100

    Restore email field and related methods in User class

--- src/main/frontend/java/com/example/application/entity/User.java
+++ src/main/frontend/java/com/example/application/entity/User.java
@@ -31,8 +31,8 @@ public class User extends AbstractEntity {
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
-   /* public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }*/
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
@@ -40,3 +40,4 @@ public class User extends AbstractEntity {
     public Role getRole() { return role; }
     public void setRole(Role role) { this.role = role; }
 }
+
